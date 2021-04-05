66e55d1e6874de507d242fc501c5980eccb436f3
thkthk97@naver.com <thkthk97@naver.com>
2021-04-05 21:11:56 +0900

    Chapter01

---
+++ Chapter01/src/main/java/com/ruby/OtherController.java
@@ -0,0 +1,10 @@
+package com.ruby;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OtherController {
+    public OtherController() {
+        System.out.println("===> OtherController 생성");
+    }
+}
--- Chapter01/src/main/java/com/rubypaper/Chapter01/Chapter01Application.java
+++ Chapter01/src/main/java/com/rubypaper/Chapter01/Chapter01Application.java
@@ -2,8 +2,10 @@ package com.rubypaper.Chapter01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rubypaper", "com.ruby"})
 public class Chapter01Application {
 
 	public static void main(String[] args) {
