047491cbf479dc40bdc38f6be2aa930dc3205169
thkthk97@naver.com <thkthk97@naver.com>
2021-04-10 01:13:37 +0900

    Chapter04 Initial Commit

---
+++ Chapter04/src/main/java/com/rubypaper/Chapter04Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter04Application.class, args);
+	}
+
+}
