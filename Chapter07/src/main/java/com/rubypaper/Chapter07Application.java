39de10a05c9b255a88875642a790781cecac10a2
thkthk97@naver.com <thkthk97@naver.com>
2021-04-19 20:39:06 +0900

    Chapter07 Initial Commit

---
+++ Chapter07/src/main/java/com/rubypaper/Chapter07Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter07Application.class, args);
+	}
+
+}
